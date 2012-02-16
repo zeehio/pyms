9afc349eb887bcc7ad881b7cc35ebdf914db3941
v.likic <v.likic@gmail.com>
2012-02-16 03:43:31 +0000

    Updated copyright notices.

--- GCMS/IO/ANDI/Function.py
+++ GCMS/IO/ANDI/Function.py
@@ -5,7 +5,7 @@ Functions for reading manufacturer specific ANDI-MS data files
  #############################################################################
  #                                                                           #
  #    PyMS software for processing of metabolomic mass-spectrometry data     #
- #    Copyright (C) 2005-2011 Vladimir Likic                                 #
+ #    Copyright (C) 2005-2012 Vladimir Likic                                 #
  #                                                                           #
  #    This program is free software; you can redistribute it and/or modify   #
  #    it under the terms of the GNU General Public License version 2 as      #
